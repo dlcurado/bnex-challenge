f61c517d8ec39af2476848313b4edba5fd21c238
dlcurado <dlcurado@gmail.com>
2023-11-22 18:26:04 -0300

    Tentando corrigir a não aplicação do migrations no backend

--- backend/start.sh
+++ backend/start.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+echo "Starting Migrations for inital Use Admin..."
+python3 manage.py migrate
+echo ====================================
+
 echo "Creating Migrations..."
 python3 manage.py makemigrations
 echo ====================================
